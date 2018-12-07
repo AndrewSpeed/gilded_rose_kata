31eff56215a4b527f8bce974f7c288021d53f752
Andrew Speed <aspeed@shopkeep.com>
2018-12-07 18:31:12 +0000

    Remove unnecessary instance variable references

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -11,15 +11,15 @@ class Normal
   end
 
   def update
-    @item.quality = if @item.quality == 0
+    item.quality = if item.quality == 0
       0
-    elsif @item.sell_in <= 0
-      @item.quality - 2
+    elsif item.sell_in <= 0
+      item.quality - 2
     else
-      @item.quality - 1
+      item.quality - 1
     end
 
-    @item.sell_in -= 1
+    item.sell_in -= 1
 
     self
   end
@@ -36,14 +36,14 @@ class AgedBrie
   end
 
   def update
-    if @item.sell_in <= 0
-      @item.quality = @item.quality + 2
-    elsif @item.sell_in > 0
-      @item.quality = @item.quality + 1
+    if item.sell_in <= 0
+      item.quality = item.quality + 2
+    elsif item.sell_in > 0
+      item.quality = item.quality + 1
     end
-    @item.quality = 50 if @item.quality >= 50
+    item.quality = 50 if item.quality >= 50
 
-    @item.sell_in = @item.sell_in - 1
+    item.sell_in = item.sell_in - 1
 
     self
   end
